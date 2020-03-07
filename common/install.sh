e2c1ffbe9202639240f470825a1811e81e643732
r3muxd <46062054+r3muxd@users.noreply.github.com>
2020-03-07 14:27:50 -0500

    SC2086 -- Double quote to prevent globbing and SC2129 -- Consider using { cmd1; cmd2; } >>

--- common/install.sh
+++ common/install.sh
@@ -3,19 +3,19 @@ set_moto_props() {
 echo "partition.oem.verified 2
 partition.vendor.verified 2
 partition.system.verified 2
-" > $MODPATH/system.prop
+" > "$MODPATH"/system.prop
 # Just to be sure, lets set those props again
 echo "resetprop partition.oem.verified 2
 resetprop partition.vendor.verified 2
 resetprop partition.system.verified 2
-" > $MODPATH/service.sh
+" > "$MODPATH"/service.sh
 }
 
 # If the vendor has the libs don't use ours
 if [ -f /vendor/lib/libdirect-coredump.so ] || [ -f /vendor/lib64/libdirect-coredump.so ] ;
 then
-	cp -rf /vendor/lib64/libdirect-coredump.so $MODPATH/system/lib64
-	cp -rf /vendor/lib/libdirect-coredump.so $MODPATH/system/lib;
+	cp -rf /vendor/lib64/libdirect-coredump.so "$MODPATH"/system/lib64
+	cp -rf /vendor/lib/libdirect-coredump.so "$MODPATH"/system/lib;
 fi
 # Originally by phhusson, Adapted by linuxandria
 if getprop ro.vendor.build.fingerprint |grep -q -e google/;
@@ -23,14 +23,14 @@ then
 	ui_print "- Google device detected!"
 	if [ -f /system/phh/google-uinput-fpc.kl ];
 	then
-		echo "cp /system/phh/google-uinput-fpc.kl /mnt/phh/keylayout/uinput-fpc.kl" >> $MODPATH/post-fs-data.sh
-		echo "chmod 0644 /mnt/phh/keylayout/uinput-fpc.kl" >> $MODPATH/post-fs-data.sh
-		echo "mount -o bind /mnt/phh/keylayout /system/usr/keylayout" >> $MODPATH/post-fs-data.sh
-		echo "restorecon -R /system/usr/keylayout" >> $MODPATH/post-fs-data.sh;
+		{ echo "cp /system/phh/google-uinput-fpc.kl /mnt/phh/keylayout/uinput-fpc.kl"
+		echo "chmod 0644 /mnt/phh/keylayout/uinput-fpc.kl"
+		echo "mount -o bind /mnt/phh/keylayout /system/usr/keylayout"
+		echo "restorecon -R /system/usr/keylayout" } >> "$MODPATH"/post-fs-data.sh;
 	fi
 fi
 if getprop ro.product.vendor.brand|grep -q -e motorola/;
 then
 	set_moto_props ;
-	echo "mount -o bind $MODDIR/vendor/bin/init.mmi.hab.sh /vendor/bin/init.mmi.hab.sh" >> $MODPATH/post-fs-data.sh
+	echo "mount -o bind $MODDIR/vendor/bin/init.mmi.hab.sh /vendor/bin/init.mmi.hab.sh" >> "$MODPATH"/post-fs-data.sh
 fi
