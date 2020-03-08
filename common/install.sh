ade40729e8dd2798309cf6a05dbc0f23485ac0ed
Alexia <45006100+linuxandria@users.noreply.github.com>
2020-03-08 14:48:38 -0400

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -3,19 +3,19 @@ set_moto_props() {
 echo "partition.oem.verified 2
 partition.vendor.verified 2
 partition.system.verified 2
-" > "$MODPATH"/system.prop
+" > $MODPATH/system.prop
 # Just to be sure, lets set those props again
 echo "resetprop partition.oem.verified 2
 resetprop partition.vendor.verified 2
 resetprop partition.system.verified 2
-" > "$MODPATH"/service.sh
+" > $MODPATH/service.sh
 }
 
 # If the vendor has the libs don't use ours
 if [ -f /vendor/lib/libdirect-coredump.so ] || [ -f /vendor/lib64/libdirect-coredump.so ] ;
 then
-	cp -rf /vendor/lib64/libdirect-coredump.so "$MODPATH"/system/lib64
-	cp -rf /vendor/lib/libdirect-coredump.so "$MODPATH"/system/lib;
+	cp -rf /vendor/lib64/libdirect-coredump.so $MODPATH/system/lib64
+	cp -rf /vendor/lib/libdirect-coredump.so $MODPATH/system/lib;
 fi
 # Originally by phhusson, Adapted by linuxandria
 if getprop ro.vendor.build.fingerprint |grep -q -e google/;
@@ -26,11 +26,11 @@ then
 	echo "chmod 0644 /mnt/phh/keylayout/uinput-fpc.kl"
 	echo "mount -o bind /mnt/phh/keylayout /system/usr/keylayout"
 	echo "restorecon -R /system/usr/keylayout"
-	} >> "$MODPATH"/service.sh;
+	} >> $MODPATH/service.sh;
 	
 fi
-if getprop ro.product.vendor.brand|grep -q -e motorola/;
+if getprop ro.product.vendor.brand| grep -q -e motorola/;
 then
 	set_moto_props ;
-	echo "mount -o bind $MODDIR/vendor/bin/init.mmi.hab.sh /vendor/bin/init.mmi.hab.sh" >> "$MODPATH"/post-fs-data.sh
+	echo "mount -o bind $MODDIR/vendor/bin/init.mmi.hab.sh /vendor/bin/init.mmi.hab.sh" >> $MODPATH/post-fs-data.sh
 fi
